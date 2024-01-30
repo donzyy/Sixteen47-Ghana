eafe82e57802ce9519c3e80897f96e9e9b236992
donzyy <61470347+donzyy@users.noreply.github.com>
2024-01-30 18:08:36 +0000

    Modification
    *Added information to the statboard in the main landing page.

--- src/Components/StatsboardLanding.jsx
+++ src/Components/StatsboardLanding.jsx
@@ -6,24 +6,24 @@ function StatsboardLanding() {
       <div className="mx-auto max-w-7xl py-12 px-6 sm:py-16 lg:px-8 lg:py-20">
         <div className="mx-auto max-w-4xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            Trusted by developers from over 80 planets
+            Quick Overview
           </h2>
           <p className="mt-3 text-xl text-SixteenBlack sm:mt-4">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat laudantium.
+            This entails everything from our production capacity to our service fulfilment
           </p>
         </div>
         <dl className="mt-10 text-center sm:mx-auto sm:grid sm:max-w-3xl sm:grid-cols-3 sm:gap-8">
           <div className="flex flex-col">
-            <dt className="order-2 mt-2 text-lg font-medium leading-6 text-SixteenBlack">Pepperoni</dt>
-            <dd className="order-1 text-5xl font-bold tracking-tight text-white">100%</dd>
+            <dt className="order-2 mt-2 text-lg font-medium leading-6 text-SixteenBlack">Production Capacity(Month)</dt>
+            <dd className="order-1 text-5xl font-bold tracking-tight text-white">12k Units</dd>
           </div>
           <div className="mt-10 flex flex-col sm:mt-0">
-            <dt className="order-2 mt-2 text-lg font-medium leading-6 text-SixteenBlack">Delivery</dt>
-            <dd className="order-1 text-5xl font-bold tracking-tight text-white">24/7</dd>
+            <dt className="order-2 mt-2 text-lg font-medium leading-6 text-SixteenBlack">Machine Capacity</dt>
+            <dd className="order-1 text-5xl font-bold tracking-tight text-white">200+</dd>
           </div>
           <div className="mt-10 flex flex-col sm:mt-0">
-            <dt className="order-2 mt-2 text-lg font-medium leading-6 text-SixteenBlack">Calories</dt>
-            <dd className="order-1 text-5xl font-bold tracking-tight text-white">100k+</dd>
+            <dt className="order-2 mt-2 text-lg font-medium leading-6 text-SixteenBlack">Direct/Indirect Staff</dt>
+            <dd className="order-1 text-5xl font-bold tracking-tight text-white">73% / 27%</dd>
           </div>
         </dl>
       </div>
