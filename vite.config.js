68c1a2c751d87d7dd6e6a9c0dd97afc0c9daa67d
donzyy <61470347+donzyy@users.noreply.github.com>
2024-02-15 08:03:22 +0000

    Modification
    *Added the sixteen-strapi cms backend to the main sixteen47-Ghana main file
    *Modified the package.json and installed the pacakge, concurrently: Concurrently is a package that allows you to run two or more terminals with a single command.
    *Modified the package.json with some commands in the script portion of the sixteen-47-Ghana so that it can run both commands in sixteen-strapi and sixteen47-Ghana concurrently.
    This is the orginal code just incase i need it:
    
    "scripts": {
        "dev": "vite",
        "build": "vite build",
        "lint": "eslint . --ext js,jsx --report-unused-disable-directives --max-warnings 0",
        "preview": "vite preview"
    
    *Modified the .env in the sixteen-strapi to use some commands i had previously commented out.
    *Modified the plugins.js file in config, located in the sixteen-strai, by uncommenting the plugin commands i had previously kept in there.

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/* export default defineConfig({
   plugins: [react()],
   server: {
     port: 4000
   }
-})
+}) */
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 4000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1337', // Replace with your Strapi server URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
