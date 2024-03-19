8434f4bfbd0658ce7417a28768bbd27454a9bb04
donzyy <61470347+donzyy@users.noreply.github.com>
2024-03-19 19:29:59 +0000

    Modification
    *Made somechanges

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import GetQuote from "./Pages/GetQuote/GetQuote"
 import Future from "./Pages/FutureOfSixteen47/Future"
 import NewsDetails2 from "./Pages/News/NewsDetails2"
 import NewsMain2 from "./Pages/News/NewsMain2"
+import ServerError from "./Pages/ServerError/ServerError"
 
 
 function App() {
@@ -22,18 +23,18 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route path="/aboutus" element={<AboutUs /> } />
-        <Route path="/contactus" element={<ContactUs /> } />
-        {/* <Route path="/faq" element={<FAQ />} /> */}
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/news" element={<NewsMain2/>} /> 
-        <Route path="/newsdetail/:id" element={<NewsDetails2/>} />
-        <Route path="/corporateresponsibility" element={<CorporateResponsibility/>}/>
-        <Route path="/team" element={<Team />}/>
-        <Route path="/quote" element={<GetQuote />}/>
-        <Route path="/future" element= {<Future />} />
-        
+        {/* <Route exact path="/" element={<LandingPage />} /> */}
+        {/* <Route path="/aboutus" element={<AboutUs /> } /> */}
+        {/* <Route path="/contactus" element={<ContactUs /> } /> */}
+        {/* <Route path="/faq" element={<FAQ />} /> */} {/* Dont uncomment this one. */}
+        {/* <Route path="/gallery" element={<Gallery />} /> */}
+        {/* <Route path="/news" element={<NewsMain2/>} />  */}
+        {/* <Route path="/newsdetail/:id" element={<NewsDetails2/>} /> */}
+        {/* <Route path="/corporateresponsibility" element={<CorporateResponsibility/>}/> */}
+        {/* <Route path="/team" element={<Team />}/> */}
+        {/* <Route path="/quote" element={<GetQuote />}/> */}
+        {/* <Route path="/future" element= {<Future />} /> */}
+        <Route path="/" element={<ServerError />} />
 
         <Route path="*" element = {< NotFound />} />
       </Routes>
